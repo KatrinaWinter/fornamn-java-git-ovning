d8a91097426fbb7346e0a2f41a5f60bb50044b41
Katrina Winter <168348366+KatrinaWinter@users.noreply.github.com>
2026-09-22 15:23:29 +0200

    Lägg till andra hälsningsraden

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej från verkstaden, Katrina");
+        System.out.println("Jag tränar på Git och GitHub!");
     }
 }
